51e26d7e83d8946a4f7078dbbd2992906cb07899
bergi <bergi@axolotlfarm.org>
2017-08-11 14:50:40 +0200

    added more media types for N-triples, removed unused media types

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const rdf = require('rdf-ext')
 const JsonLdParser = require('rdf-parser-jsonld')
 const N3Parser = require('rdf-parser-n3')
-// const RdfXmlParser = require('rdf-parser-rdfxml')
 const JsonLdSerializer = require('rdf-serializer-jsonld-ext')
-// const N3Serializer = require('rdf-serializer-n3')
 const NTriplesSerializer = require('rdf-serializer-ntriples')
-// const SparqlUpdateSerializer = require('rdf-serializer-sparql-update')
 
 function register (handler, format, instance) {
   if (!(format in handler)) {
@@ -20,15 +17,13 @@ function mixin (object) {
 
   register(object.parsers, 'application/ld+json', new JsonLdParser({factory: rdf}))
   register(object.parsers, 'application/n-triples', new N3Parser({factory: rdf}))
-  // register(object.parsers, 'application/rdf+xml', RdfXmlParser)
   register(object.parsers, 'text/n3', new N3Parser({factory: rdf}))
   register(object.parsers, 'text/turtle', new N3Parser({factory: rdf}))
 
   register(object.serializers, 'application/ld+json', new JsonLdSerializer({outputFormat: 'string'}))
   register(object.serializers, 'application/n-triples', new NTriplesSerializer())
-  // register(object.serializers, 'application/sparql-update', SparqlUpdateSerializer)
-  // register(object.serializers, 'text/n3', N3Serializer)
-  // register(object.serializers, 'text/turtle', N3Serializer)
+  register(object.serializers, 'text/n3', new NTriplesSerializer())
+  register(object.serializers, 'text/turtle', new NTriplesSerializer())
 
   return object
 }
--- test/test.js
+++ test/test.js
@@ -4,11 +4,8 @@ const assert = require('assert')
 const rdf = require('rdf-ext')
 const JsonLdParser = require('rdf-parser-jsonld')
 const N3Parser = require('rdf-parser-n3')
-// const RdfXmlParser = require('rdf-parser-rdfxml')
 const JsonLdSerializer = require('rdf-serializer-jsonld-ext')
-// const N3Serializer = require('rdf-serializer-n3')
 const NTriplesSerializer = require('rdf-serializer-ntriples')
-// const SparqlUpdateSerializer = require('rdf-serializer-sparql-update')
 
 var formats = require('..')(rdf)
 
@@ -25,7 +22,6 @@ describe('rdf-formats-common', () => {
 
       assert.notEqual(mimeTypes.indexOf('application/ld+json'), -1)
       assert.notEqual(mimeTypes.indexOf('application/n-triples'), -1)
-      // assert.notEqual(mimeTypes.indexOf('application/rdf+xml'), -1)
       assert.notEqual(mimeTypes.indexOf('text/n3'), -1)
       assert.notEqual(mimeTypes.indexOf('text/turtle'), -1)
     })
@@ -33,7 +29,6 @@ describe('rdf-formats-common', () => {
     it('.find should find parsers for standard formats', () => {
       assert(formats.parsers.find('application/ld+json') instanceof JsonLdParser)
       assert(formats.parsers.find('application/n-triples') instanceof N3Parser)
-      // assert.equal(formats.parsers.find('application/rdf+xml'), RdfXmlParser)
       assert(formats.parsers.find('text/n3') instanceof N3Parser)
       assert(formats.parsers.find('text/turtle') instanceof N3Parser)
     })
@@ -49,19 +44,17 @@ describe('rdf-formats-common', () => {
     it('.list should list all mime types', () => {
       let mimeTypes = formats.serializers.list()
 
-      // assert.notEqual(mimeTypes.indexOf('application/ld+json'), -1)
+      assert.notEqual(mimeTypes.indexOf('application/ld+json'), -1)
       assert.notEqual(mimeTypes.indexOf('application/n-triples'), -1)
-      // assert.notEqual(mimeTypes.indexOf('application/sparql-update'), -1)
-      // assert.notEqual(mimeTypes.indexOf('text/n3'), -1)
-      // assert.notEqual(mimeTypes.indexOf('text/turtle'), -1)
+      assert.notEqual(mimeTypes.indexOf('text/n3'), -1)
+      assert.notEqual(mimeTypes.indexOf('text/turtle'), -1)
     })
 
     it('.find should find parsers for standard formats', () => {
       assert(formats.serializers.find('application/ld+json') instanceof JsonLdSerializer)
       assert(formats.serializers.find('application/n-triples') instanceof NTriplesSerializer)
-      // assert.equal(formats.serializers.find('application/sparql-update'), SparqlUpdateSerializer)
-      // assert.equal(formats.serializers.find('text/n3'), N3Serializer)
-      // assert.equal(formats.serializers.find('text/turtle'), N3Serializer)
+      assert(formats.serializers.find('text/n3') instanceof NTriplesSerializer)
+      assert(formats.serializers.find('text/turtle') instanceof NTriplesSerializer)
     })
   })
 })
